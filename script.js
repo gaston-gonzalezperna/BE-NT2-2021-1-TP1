74d02e3f70368d15e4ca0eb1c402d52e24856ad0
Gaston Gonzalez Perna <ggonzalezperna@gmail.com>
2021-04-06 23:55:44 -0300

    Armado de TP1

--- script.js
+++ script.js
@@ -10,6 +10,32 @@ const classNames = {
   const itemCountSpan = document.getElementById('item-count')
   const uncheckedCountSpan = document.getElementById('unchecked-count')
   
+  let contadorElementos = 0
+
   function addTodo() {
-    alert('Boton Add TODO clickeado!')
+    var dato = window.prompt("Introduce tu Tarea a Realizar")
+    if(dato !== null && dato !== ''){
+      agregarElementoTODO(dato, list)
+    }
+    contarCheckbox()
+  }
+
+  function agregarElementoTODO(dato, list){
+    var elementoLista = document.createElement("li")
+    var checkbox = document.createElement("input")
+    checkbox.type = "checkbox"
+    elementoLista.append(checkbox)
+    elementoLista.append(dato)
+    list.append(elementoLista)
+    sumarElemento()
+  }
+
+  function sumarElemento(){
+    contadorElementos++
+    itemCountSpan.innerHTML = contadorElementos
+  }
+
+  //No estoy seguro como hacer para que esto cargue sin tener que hacer click en el boton de agregar elemento, pero bueno, funciona asi
+  function contarCheckbox(){
+    uncheckedCountSpan.innerHTML = document.querySelectorAll('input[type=checkbox]:not(:checked)').length
   }
